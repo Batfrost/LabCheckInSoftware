793e03bbba4fe0cff983c8d13094738726ad5776
Batfrost <batmantis2003@gmail.com>
2025-06-06 12:13:28 -0600

    Implemented most of Manager Window and some of Lookup Window. Both also include a timer that will automatically close the window after some time if no mouse movement detected.

--- LabCheckInSoftware/LookupWindow.xaml.cs
+++ LabCheckInSoftware/LookupWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TWLogging
 {
@@ -19,9 +20,59 @@ namespace TWLogging
     /// </summary>
     public partial class LookupWindow : Window
     {
+        private DispatcherTimer CloseWindowTimer;
+        private int TimeCounter = 0;
+        //Lock for when both mouse movement and tick events change TimeCounter value
+        private static readonly object Lock = new object();
+
         public LookupWindow()
         {
             InitializeComponent();
+
+            //Get the SignUpWindow's specific custom info from the SettingsController
+            string[] InfoFields = SettingsController.GetInfoFields();
+            if (InfoFields[0] != null && InfoFields[0].Length > 0)
+            {
+                InfoField1Label.Content = InfoFields[0] + ": ";
+                InfoField1Box.IsEnabled = true;
+            }
+            if (InfoFields[1] != null && InfoFields[1].Length > 0)
+            {
+                InfoField2Label.Content = InfoFields[1] + ": ";
+                InfoField2Box.IsEnabled = true;
+            }
+
+            //Start a timer that resets everytime the mouse is moved, and when it reaches 30 seconds, closes this window.
+            CloseWindowTimer = new DispatcherTimer();
+            CloseWindowTimer.Tick += CloseWindowTimer_Tick!;
+            CloseWindowTimer.Interval = TimeSpan.FromSeconds(1);
+            CloseWindowTimer.Start();
+        }
+
+
+
+        //Tick event for when the CloseWindowTimer will close the window
+        private void CloseWindowTimer_Tick(object sender, EventArgs e)
+        {
+            lock (Lock)
+            {
+                TimeCounter++;
+                TimerLabel.Content = (30 - TimeCounter).ToString();
+                if (TimeCounter == 30)
+                {
+                    CloseWindowTimer.Stop();
+                    this.Close();
+                }
+            }
+        }
+
+        //Everytime the window detects mouse movement, reset the close window timer
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lock (Lock)
+            {
+                TimeCounter = 0;
+            }
         }
     }
 }
--- LabCheckInSoftware/ManagerWindow.xaml.cs
+++ LabCheckInSoftware/ManagerWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace TWLogging
 {
@@ -19,9 +8,92 @@ namespace TWLogging
     /// </summary>
     public partial class ManagerWindow : Window
     {
+        private DispatcherTimer CloseWindowTimer;
+        private int TimeCounter = 0;
+        //Lock for when both mouse movement and tick events change TimeCounter value
+        private static readonly object Lock = new object();
+
         public ManagerWindow()
         {
             InitializeComponent();
+
+            //Start a timer that resets everytime the mouse is moved, and when it reaches 30 seconds, closes this window.
+            CloseWindowTimer = new DispatcherTimer();
+            CloseWindowTimer.Tick += CloseWindowTimer_Tick!;
+            CloseWindowTimer.Interval = TimeSpan.FromSeconds(1);
+            CloseWindowTimer.Start();
+        }
+
+        //Takes user to Settings Window
+        private void ChangeSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Initialize the Settings Window to match this windows size settings
+            var SW = new SettingsWindow()
+            {
+                WindowStartupLocation = this.WindowStartupLocation,
+                Width = this.Width,
+                Height = this.Height,
+                WindowState = this.WindowState,
+                WindowStyle = this.WindowStyle
+            };
+            SW.Show();
+            this.Close();
+        }
+
+        //Takes user to AttendanceTracker Window
+        private void AttendanceTrackerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Initialize the AttendanceTracker Window to match this windows size settings
+            var ATW = new AttendanceTrackerWindow()
+            {
+                WindowStartupLocation = this.WindowStartupLocation,
+                Width = this.Width,
+                Height = this.Height,
+                WindowState = this.WindowState,
+                WindowStyle = this.WindowStyle
+            };
+            ATW.Show();
+            this.Close();
+        }
+
+        //Takes user to Lookup Window
+        private void LookupButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Initialize the Lookup Window to match this windows size settings
+            var LW = new LookupWindow()
+            {
+                WindowStartupLocation = this.WindowStartupLocation,
+                Width = this.Width,
+                Height = this.Height,
+                WindowState = this.WindowState,
+                WindowStyle = this.WindowStyle
+            };
+            LW.Show();
+            this.Close();
+        }
+
+        //Tick event for when the CloseWindowTimer will close the window
+        private void CloseWindowTimer_Tick(object sender, EventArgs e)
+        {
+            lock (Lock)
+            {
+                TimeCounter++;
+                TimerLabel.Content = (30 - TimeCounter).ToString();
+                if (TimeCounter == 30)
+                {
+                    CloseWindowTimer.Stop();
+                    this.Close();
+                }
+            }
+        }
+
+        //Everytime the window detects mouse movement, reset the close window timer
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lock (Lock)
+            {
+                TimeCounter = 0;
+            }
         }
     }
 }
